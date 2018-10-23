adb4c56766b4dae15814164c8079a251b2bc19f5
sheylao <sheypr85@gmail.com>
2018-10-22 19:13:59 -0500

    Adding index html

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//set variables 
+//4 crystals to act as buttons for function
+    // create values for crystals
+// create random number for user to guess answer to
+//add amount of points to a (card) adding what user is clicking
+// when it gets to the number stop (lose or win) and restart
+//
